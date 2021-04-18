11cb7b1c27900989f46bcbb00c36dd7ca99c7ec7
ssruoh <ssruoh@gmail.com>
2021-04-18 19:51:04 +0000

    Add toast template and view, add block.super to templates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -16,6 +18,7 @@ def add_to_bag(request, item_id):
     Add selected quantity of product to bag
     """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -24,6 +27,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'{product.name} successfully added to bag!')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
