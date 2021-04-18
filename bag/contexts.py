86c6dd1e76cf82cebdb52df6119c981561c2781f
ssruoh <ssruoh@gmail.com>
2021-04-18 00:46:55 +0000

    Fix bug showing incorrect total purchase cost

--- bag/contexts.py
+++ bag/contexts.py
@@ -15,7 +15,7 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity + product.price
+        total += quantity * product.price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
@@ -24,7 +24,7 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
