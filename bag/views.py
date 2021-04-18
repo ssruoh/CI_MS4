1d9dbc1cac44dfc10036122f79d0b375d0c3fb43
ssruoh <ssruoh@gmail.com>
2021-04-18 21:09:00 +0000

    Add toast messages for updating and deleting from bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -18,7 +18,7 @@ def add_to_bag(request, item_id):
     Add selected quantity of product to bag
     """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -37,14 +37,16 @@ def update_bag(request, item_id):
     """
     Update quantity of products in bag
     """
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated quantity of {product.name}!')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from bag!')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -55,11 +57,14 @@ def delete_from_bag(request, item_id):
     Delete item from bag
     """
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from bag!')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing {e} from bag!')
         return HttpResponse(status=500)
