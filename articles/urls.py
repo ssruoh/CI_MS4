6eb16deaf9740ecc451df8fbf8e7b3fb6e3dd954
ssruoh <ssruoh@gmail.com>
2021-04-30 00:18:30 +0000

    Create add_article form, template, start on view

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.all_articles, name='articles'),
     path('<article_id>', views.article_detail, name='article_detail'),
+    path('add_article/', views.add_article, name='add_article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from .models import Article
+from .forms import ArticleForm
 
 
 def all_articles(request):
@@ -27,3 +29,42 @@ def article_detail(request, article_id):
     }
 
     return render(request, 'articles/article_detail.html', context)
+
+
+@login_required
+def add_article(request):
+    """
+    View to add an article
+    """
+
+    if request.method == 'POST':
+        article_form_data = {
+            'title': request.POST['post_code'],
+            'description': request.POST['town_or_city'],
+            'article_body': request.POST['county'],
+            'image': request.POST['image'],
+        }
+        article_form = ArticleForm(article_form_data)
+
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
+            for item_id, quantity in bag.items():
+                product = Product.objects.get(id=item_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                )
+                order_line_item.save()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(
+                request, 'There was an issue with your form. \
+                    Please recheck the fields.')
+
+    return render(request, 'articles/add_article.html')
