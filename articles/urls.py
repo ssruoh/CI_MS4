1275ded51f53162e7c1f3e269392b061942cac14
ssruoh <ssruoh@gmail.com>
2021-05-02 00:33:19 +0000

    Fix bug causing add_article to not render, add edit_article view & template

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.all_articles, name='articles'),
-    path('<article_id>', views.article_detail, name='article_detail'),
     path('add_article/', views.add_article, name='add_article'),
+    path('<article_id>/', views.article_detail, name='article_detail'),
+    path('edit_article/<article_id>/', views.edit_article, name='edit_article'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -49,10 +49,12 @@ def add_article(request):
             article = article_form.save(commit=False)
             """ Use request.user to attach current user as author of article,
             https://stackoverflow.com/questions/19799941/attaching-a-current-user-object-to-django-form
+            https://stackoverflow.com/questions/8466768/using-request-user-with-django-modelform
             """
             article.author = request.user
             article.save()
-            return redirect(reverse('add_article'))
+            messages.success(request, 'Article added!')
+            return redirect(reverse('article_detail', args=[article.id]))
         else:
             messages.error(
                 request, 'There was a problem with the form. Please check the fields.')
@@ -64,3 +66,42 @@ def add_article(request):
             'article_form': article_form,
         }
         return render(request, template, context)
+
+
+@login_required
+def edit_article(request, article_id):
+    """
+    View to edit an article
+    """
+    article = get_object_or_404(Article, pk=article_id)
+    if request.method == 'POST':
+        article_form_data = {
+            'title': request.POST['title'],
+            'description': request.POST['description'],
+            'article_body': request.POST['article_body'],
+            'image': request.POST['image'],
+        }
+        article_form = ArticleForm(
+            article_form_data, instance=article)
+        if article_form.is_valid():
+            article_form.author = request.user
+            article_form.save()
+            messages.success(request, 'Article updated!')
+            return redirect(reverse('article_detail', args=[article.id]))
+        else:
+            messages.error(
+                request, 'There was a problem with the form. Please check the fields.')
+    else:
+        article = get_object_or_404(Article, pk=article_id)
+        article_form = ArticleForm(instance=article)
+        messages.info(
+            request, 'You are editing an article.')
+
+        template = 'articles/edit_article.html'
+
+    context = {
+        'article_form': article_form,
+        'article': article,
+    }
+
+    return render(request, template, context)
